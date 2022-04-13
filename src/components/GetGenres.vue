de4fa3b6ccc93f2ec97b39ddda6273d6479c8cb9
BIRDISS-kawtar <birdisskawtar229@gmail.com>
2022-04-13 16:52:17 +0200

    Get Genres from the API

---
+++ src/components/GetGenres.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card text-center m-3">
+    <h5 class="card-header">GET Request for genres with Error Handling</h5>
+    <div class="card-body">Genres are: {{genres}}</div>
+    <div class="card-body">Error message: {{errorMessage}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "get-genres-of-movies",
+  data() {
+    return {
+      genres: new Array(),
+      errorMessage: null
+    };
+  },
+  created() {
+    // GET request using fetch with error handling
+    fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=18f0e56333fe7988fb15f351af41f492&language=en-US")
+      .then(async response => {
+        const data = await response.json();
+
+        // check for error response
+        if (!response.ok) {
+          // get error message from body or default to response statusText
+          const error = (data && data.message) || response.statusText;
+          return Promise.reject(error);
+        }
+        // continue if there is any error
+        // stock values of genres in an array to be used later
+        for(const genre of data.genres){
+            this.genres.push(genre.name);
+        }
+
+      })
+      .catch(error => {
+        this.errorMessage = error;
+        console.error("There was an error!", error);
+      });
+  }
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script>
-
+// The content of the home view is splitted into components 
+import genres from "@/components/GetGenres.vue";
+// Each imported child component must be registred inside the parent component
+export default {
+  name: "home",
+  components: {
+   GetGenres
+  }
+};
 </script>
 
 <template>
 <div>
     <h1>Welcome to the home page!</h1>
+    <p>genres</p>
 </div>
 </template>
