e348fae7834486af6f8c0150abff53a90f044693
Aymane <h4ckyh4wk@localhost.localdomain>
2022-04-23 20:32:24 +0200

    adding MovieDetails.vue and modifying route and style.css

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,39 @@ import appNavbar from "@/components/appNavbar.vue";
 import appFooter from "@/components/appFooter.vue"
 import createStore from '@/store/index.js'
 export default{
+
+  //lifecycle hooks
+  mounted(){
+    //this.addExternalScripts();
+  },
   components: {
      'appNavbar': appNavbar,
      'appFooter': appFooter
   },
+  methods: {
+    addExternalScripts(){
+      let jQuery = document.createElement('script');
+      jQuery.setAttribute('src', 'src/assets/js/jquery.js');
+      document.head.appendChild(jQuery);
+
+      let plugins = document.createElement('script');
+      plugins.setAttribute('src', 'src/assets/js/plugins.js');
+      document.head.appendChild(plugins);
+
+      let plugins2 = document.createElement('script');
+      plugins2.setAttribute('src', 'src/assets/js/plugins2.js');
+      document.head.appendChild(plugins2);
+
+      let custom = document.createElement('script');
+      custom.setAttribute('src', 'src/assets/js/custom.js');
+      document.head.appendChild(custom);
+    },
+  }
 };
 </script>
 
 <style>
 @import "./assets/css/style.css";
+/* @import "./assets/css/plugins.css"; */
+
 </style>
