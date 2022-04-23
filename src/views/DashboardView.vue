008705e99769bb462e1cd43919b3bae6cde7838a
BIRDISS-kawtar <birdisskawtar229@gmail.com>
2022-04-23 16:00:12 +0200

    modifying dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -124,6 +124,7 @@ export default {
 				console.log("movie list "+typeof this.userFavoriteMoviesList);
 				if(isProxy(this.userFavoriteMoviesList)){ //this If() block is not really necessary
 					var userFavoriteMoviesListConverted = toRaw(this.userFavoriteMoviesList);
+					//this.$store.commit();
 				}	
 				console.log(userFavoriteMoviesListConverted);
 			
