9d75f81d0827f6be5b39f748d5d62a8726340c3b
hamzatamry <hamzatamry@outlook.fr>
2022-04-23 15:30:01 +0200

    store updated

--- src/store/index.js
+++ src/store/index.js
@@ -4,28 +4,31 @@ export default createStore({
   // The shared data
   state: {
     apiKey : '18f0e56333fe7988fb15f351af41f492',
-    genres : new Array(),
+    message: null
   },
   // For getting the state
   getters: {
-    getApiKey (state) {
+    getApiKey(state) {
       // state variable contains our state data
       return state.apiKey;
     },
-    getGenres (state){
-      return state.genres;
+
+    getMessage(state) {
+      return state.message;
     }
   },
+
   // For changing the state (Synchronous)
   mutations: {
-    updateGenres(state, genres_array) {
-      
-          state.genres = [...genres_array]; // Clone an array     
-   }
+    setMessage(state, message) {
+      state.message = message;
+    }
   },
+
   // For changing the state (Asynchronous)
   actions: {
   },
+
   modules: {
   }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,22 @@
 <template>
 <!------------- Title Block------------->
 <div class="hero common-hero">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="hero-ct">
-					<h1> Films DB, All your favourite movies in one place ! </h1>
-          			<GetGenres /> <!-- GetGenres doesn't display anything but it's called here to get the list of genres-->
-				</div>
-			</div>
-		</div>
-	</div>
+	
 </div>
 
 <!------------- END : Title Block------------->
-<TrendingMovies />
+<MovieList />
 </template>
 
 <script>
-// The content of the home view is splitted into components
-import GetGenres from "@/components/GetGenres.vue";
-import GenreMovies from "@/components/GenreMovies.vue";
-import TrendingMovies from "./TrendingMovies.vue"
-import TopRatedMovies from "./TopRatedMovies.vue"
-
+// The content of the home view is splitted into components 
+//import GetGenres from "@/components/GetGenres.vue";
+import MovieList from "@/components/MovieList.vue";
 // Each imported child component must be registred inside the parent component
 export default {
   name: "home",
   components: {
-   GetGenres,
-   TrendingMovies,
-   TopRatedMovies,
-   GenreMovies
+	  MovieList
   }
 };
 </script>
